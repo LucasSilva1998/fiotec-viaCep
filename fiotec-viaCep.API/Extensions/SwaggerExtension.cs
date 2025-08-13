b3caa6b4bfbf9e6abf5b1b926e822172b5fa8b8d
Lucas Pereira da Silva <lucassilva@fiotec.fiocruz.br>
2025-08-13 17:58:23 -0300

    Add project files.

---
+++ fiotec-viaCep.API/Extensions/SwaggerExtension.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace fiotec_viaCep.API.Extensions
+{
+    public static class SwaggerExtension
+    {
+        public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Fiotec - ViaCEP API - Consulta de Endereços",
+                    Version = "v1",
+                    Description = "API responsável por consultar endereços a partir do CEP utilizando o serviço ViaCEP.",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Lucas Pereira",
+                        Email = "lucassilva@fiotec.fiocruz.com",
+                        Url = new Uri("https://github.com/LucasSilva1998/FiotecProva")
+                    }
+                });
+             
+            });
+
+            return services;
+        }
+    }
+}
+
+
+
